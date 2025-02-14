73cc43673ec026263f85299631b43f0d30bcf0da
lunDreame <87955512+lunDreame@users.noreply.github.com>
2025-02-14 23:20:12 +0900

    Update const.py

--- custom_components/kocom_wallpad/const.py
+++ custom_components/kocom_wallpad/const.py
@@ -27,7 +27,7 @@ DEFAULT_PORT = 8899
 BRAND_NAME = "Kocom"
 MANUFACTURER = "KOCOM Co., Ltd"
 MODEL = "Smart Wallpad"
-SW_VERSION = "1.1.7"
+SW_VERSION = "1.1.8"
 
 DEVICE_TYPE = "device_type"
 ROOM_ID = "room_id"
